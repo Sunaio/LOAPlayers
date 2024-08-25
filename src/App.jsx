d0a8702cfb6c8a57f3500680d717828c76823ef7
Sunaio <finalsecond21@gmail.com>
2024-08-25 17:44:42 -0400

    animations added, background video added, homepage done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,70 @@
 import './App.css'
 import Cards from './components/regionCard.jsx'
+import {motion} from "framer-motion"
+import videobg from  "./assets/videobg.mp4"
+
+const fadeInAnimations = {
+  initial: {
+      opacity: 0,
+      x: -100,
+  },
+  animate:  {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "tween", duration: 1,
+        delay: 0.5,
+      },
+  },
+};
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>LOA Players</h1>
-        <p>Interactive Player Database</p>
-      </div>
-      <h1>Select a region</h1>
-      <div className="regionSelect">
-        <Cards
-          imageURL = "images/deadge.png"
-          title = "NA West"
-          desc = "Player Data from NA West Region"
-        />
-        <Cards
-          imageURL = "images/thaemine.jpeg"
-          title = "NA East"
-          desc = "Player Data from NA East Region"
-        />
-        <Cards
-          imageURL = "images/brel.jpg"
-          title = "EU Central"
-          desc = "Player Data from EU Central Region"
-        />
+      <div className="backgroundVid">
+          <video src={videobg} autoPlay loop muted/>
+        <div className="content">
+          <motion.div className="titleHead"
+            variants={fadeInAnimations}
+            initial="initial"
+            whileInView="animate">
+          <h1>LOA Players</h1>
+          <p>Interactive Player Database</p>
+          </motion.div>
+          <motion.h1
+            variants={fadeInAnimations}
+            initial= {{
+            opacity: 0,
+            y: -50,
+            }}
+          whileInView={{
+            opacity: 1,
+            y: 0,
+            transition: {
+              type: "tween", duration: 1,
+              delay: 1.7,
+            },
+          }}>
+          Select a region</motion.h1>
+          <div className="regionSelect">
+            <Cards
+              imageURL = "images/deadge.png"
+              title = "NA West"
+              desc = "Player Data from NA West Region"
+            />
+            <Cards
+              imageURL = "images/thaemine.jpeg"
+              title = "NA East"
+              desc = "Player Data from NA East Region"
+            />
+            <Cards
+              imageURL = "images/brel.jpg"
+              title = "EU Central"
+              desc = "Player Data from EU Central Region"
+            />
+          </div>
+        </div>
       </div>
     </>
   )
--- src/components/regionCard.jsx
+++ src/components/regionCard.jsx
@@ -1,14 +1,35 @@
+import {motion} from "framer-motion";
+
+const fadeInAnimations = {
+    initial: {
+        opacity: 0,
+        y: -100,
+    },
+    animate: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: "tween", duration: 1,
+            delay: 3,
+        },
+    },
+};
 
 function regionCard(props) {
     return (
         //Card component used for 3 different game regions (NAW, NAE, EUC)
         //On click should route to their specific region.
         <div className="cardContainer">
-            <div className="card">
+            <motion.div className="card" 
+                variants={fadeInAnimations}
+                initial="initial"
+                whileInView="animate"
+                whileHover={{scale:1.3}}
+                transition= {{type: "spring", bounce: 0.4}}>
                 <img src = {props.imageURL} alt = '' />
                 <h2>{props.title}</h2>
                 <p>{props.desc}</p>
-            </div>
+            </motion.div>
         </div>
     );
 }
